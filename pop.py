b4179c425c2926460149d63e13e092a014eddeb7
vamsikrishna-spec <24331a05e7@mvgrce.edu.in>
2026-02-27 14:41:11 +0530

    Add files via upload

---
+++ pop.py
@@ -0,0 +1,12 @@
+data = {"a":1,"b":2,"c":3,"d":4}
+k = data.keys()
+print(k)
+v = data.values()
+print(v)
+i = data.items()
+print(i)
+p = data.pop("d")
+print(p)
+print(data)
+del data["c"]
+print(data)
